6f8f32dbfc754ca5af96797f2952f0119dcf0722
Evgenii <evgeniy@nspcc.ru>
2019-03-02 23:18:43 +0300

    add some more tests

--- exec_test.go
+++ exec_test.go
@@ -11,6 +11,7 @@ func TestExec(t *testing.T) {
 		inner struct {
 			Field2 uint64
 			Field1 map[string]int
+			Field3 []int8
 		}
 		tt struct {
 			Data []inner
@@ -24,11 +25,16 @@ func TestExec(t *testing.T) {
 	)
 
 	Convey("exec", t, func() {
-		cmds = MustCompile(Parse(".Field2"))
 		input = inner{Field2: 10, Field1: map[string]int{"1": 1, "2": 2, "3": 3}}
+		cmds = MustCompile(Parse(".Field2"))
 		r, err = Exec(cmds, input)
 		So(err, ShouldBeNil)
 		So(r, ShouldResemble, uint64(10))
+
+		cmds = MustCompile(Parse("[1]"))
+		r, err = Exec(cmds, input)
+		So(err, ShouldNotBeNil)
+		So(r, ShouldBeNil)
 
 		cmds = MustCompile(Parse(".Field1 !len"))
 		r, err = Exec(cmds, input)
@@ -38,6 +44,16 @@ func TestExec(t *testing.T) {
 		cmds = MustCompile(Parse(".Field1 !keys"))
 		r, err = Exec(cmds, input)
 		So(err, ShouldBeNil)
+		So(r, ShouldHaveSameTypeAs, []string{})
+		So(r, ShouldHaveLength, 3)
+		for _, v := range r.([]string) {
+			So(v, ShouldBeIn, []string{"1", "2", "3"})
+		}
+
+		cmds = MustCompile(Parse(".Field1 [1]"))
+		r, err = Exec(cmds, input)
+		So(err, ShouldBeNil)
+		So(r, ShouldEqual, 1)
 
 		cmds = MustCompile(Parse(".Field1 !values"))
 		r, err = Exec(cmds, input)
@@ -48,11 +64,32 @@ func TestExec(t *testing.T) {
 			So(v, ShouldBeIn, []int{1, 2, 3})
 		}
 
-		cmds = MustCompile(Parse("[] .Field2"))
+		input = inner{Field3: []int8{1,5}}
+		cmds = MustCompile(Parse(".Field3 [1]"))
+		r, err = Exec(cmds, input)
+		So(err, ShouldBeNil)
+		So(r, ShouldEqual, 5)
+
+		cmds = MustCompile(Parse(".Field3 [a]"))
+		r, err = Exec(cmds, input)
+		So(err, ShouldNotBeNil)
+		So(r, ShouldBeNil)
+
 		input = []inner{{Field2: 10}, {Field2: 20}}
+		cmds = MustCompile(Parse("[] .Field2"))
 		r, err = Exec(cmds, input)
 		So(err, ShouldBeNil)
 		So(r, ShouldResemble, []uint64{10, 20})
+
+		cmds = MustCompile(Parse("!values !flatten"))
+		r, err = Exec(cmds, input)
+		So(err, ShouldNotBeNil)
+		So(r, ShouldBeNil)
+
+		cmds = MustCompile(Parse(".Field1"))
+		r, err = Exec(cmds, input)
+		So(err, ShouldNotBeNil)
+		So(r, ShouldBeNil)
 
 		cmds = MustCompile(Parse("!values !flatten"))
 		input = map[int][]int{1: {1, 2, 3}, 2: {4, 5, 6}}
--- parser_test.go
+++ parser_test.go
@@ -15,35 +15,35 @@ var testCases = []struct {
 		cmds: []command{field("Data"), builtin("flatten")},
 	},
 	{
-		s:    ".Data [1] [2]",
-		cmds: []command{field("Data"), index("1"), index("2")},
+		s:    ".Data [1] [2] []",
+		cmds: []command{field("Data"), index("1"), index("2"), array()},
+	},
+	{
+		s:    `[mapkey]`,
+		cmds: []command{index("mapkey")},
 	},
 }
 
 var errorCases = []string{
 	"!kek",
 	"!",
-	"[",
+	".f !",
+	"[2] [",
 	"]",
+	"1",
 }
 
 func TestParse(t *testing.T) {
-	var (
-		cmds []command
-		err  error
-	)
+	var cmds []command
 
 	Convey("parse", t, func() {
 		for _, tc := range testCases {
-			cmds, err = Parse(tc.s)
-			So(err, ShouldBeNil)
+			cmds = MustCompile(Parse(tc.s))
 			So(cmds, ShouldResemble, tc.cmds)
 		}
 
 		for _, tc := range errorCases {
-			cmds, err = Parse(tc)
-			So(cmds, ShouldBeNil)
-			So(err, ShouldNotBeNil)
+			So(func() { MustCompile(Parse(tc)) }, ShouldPanic)
 		}
 	})
 }
